94fda8928f1c6fc6500bca8e1dfbb07c20b65c7c
Mathieu Benoit <mathben963@gmail.com>
2016-07-03 13:55:46 -0400

    WIP manual : add array format into paragraph of bullet point

--- src/web/resources/js/tl_module/manual_ctrl/manual_ctrl.js
+++ src/web/resources/js/tl_module/manual_ctrl/manual_ctrl.js
@@ -15,6 +15,34 @@ characterApp.controller("manual_ctrl", ["$scope", "$q", "$http", "$window", /*"$
     return title.replace(/\s+/g, '');
   };
 
+  $scope.formatHtmlDescription = function (desc) {
+    // format html from string or array
+    var response;
+    if (Object.prototype.toString.call(desc) === '[object Array]') {
+      // each item from string is a paragraph
+      response = "";
+
+      var arrayLength = desc.length;
+      for (var i = 0; i < arrayLength; i++) {
+        var desc_item = desc[i];
+        if (typeof desc_item === 'string') {
+          response += "<p>" + desc_item + "</p>";
+        } else if (Object.prototype.toString.call(desc_item) === '[object Array]') {
+          // each array in array is bullet list
+          var arrayLength_2 = desc_item.length;
+          response += "<ul>";
+          for (var j = 0; j < arrayLength_2; j++) {
+            response += "<li>" + desc_item[j] + "</li>";
+          }
+          response += "</ul>";
+        }
+      }
+    } else {
+      response = desc;
+    }
+    return response;
+  };
+
   $http.get("/cmd/rule").success(
     function (data/*, status, headers, config*/) {
       $scope.manual = data.manual;
